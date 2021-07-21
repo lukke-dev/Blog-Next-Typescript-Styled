592edd36be33bf2ef848652b4dcca20fc9f98a2a
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-07-21 09:39:55 -0300

    add head

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,7 +1,9 @@
+import Head from 'next/head';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { MainContainer } from '../../components/MainContainer';
 import { PostCard } from '../../components/PostCard';
+import { SITE_NAME } from '../../config/app-config';
 import { PostData } from '../../domain/posts/post';
 import { Container } from './styled';
 
@@ -12,6 +14,10 @@ export type HomePageProps = {
 export default function HomePage({ posts }: HomePageProps) {
   return (
     <>
+      <Head>
+        <title>{SITE_NAME}</title>
+        <meta name="description" content="Este é o meu blog de tecnologia" />
+      </Head>
       <Header />
       <MainContainer>
         <Container>
