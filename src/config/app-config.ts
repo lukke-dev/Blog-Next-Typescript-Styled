b3092e7f13169a5de46aca3ade02348336856a9a
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-07-21 09:20:29 -0300

    add comments

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://guarded-hamlet-72412.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Lucas dos Santos';
-export const SITE_URL = '';
+export const SITE_URL = 'https://elated-poincare-9b4b09.netlify.app';
